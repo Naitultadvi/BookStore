65fbc9df1dfcfaf6b6026eee075b0c8783dc01dc
Naitultadvi <127601069+Naitultadvi@users.noreply.github.com>
2023-08-17 15:49:44 +0530

    Add files via upload

---
+++ src/style/Globalstyle.js
@@ -0,0 +1,10 @@
+export const Globalstyle = {
+    navbar:{
+        padding:10,
+          display:"flex",
+          width:160,
+          fontSize:20,
+          marginRight:10
+    }
+}
+export default Globalstyle
